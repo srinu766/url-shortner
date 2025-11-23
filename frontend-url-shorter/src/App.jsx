f72198d47b3ef181063673b0867328c50911bea3
srinu <sopparisrinu637@gmail.com>
2025-11-23 13:27:57 +0530

    HashRouter

--- frontend-url-shorter/src/App.jsx
+++ frontend-url-shorter/src/App.jsx
@@ -1,7 +1,7 @@
 
 
 
-import {HasherRouter, BrowserRouter, Routes, Route } from "react-router-dom";
+import {HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Stats from "./pages/Stats";
 import Redirect from "./pages/Redirect";
@@ -9,7 +9,7 @@ import NotFound from "./pages/NotFound";
 import { ToastContainer } from "react-toastify";
 
 const App = () => (
-  <HasherRouter>
+  <HashRouter>
     <ToastContainer />
     <Routes>
       <Route path="/" element={<Dashboard />} />
@@ -19,7 +19,7 @@ const App = () => (
       <Route path="/r/:code" element={<Redirect />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
-  </HasherRouter>
+  </HashRouter>
 );
 
 export default App;
